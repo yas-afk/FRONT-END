416167c4daf05ec6ec94182710dbe8879e5372de
yas-afk <yasminsoaresfarias32@gmail.com>
2024-11-14 17:22:22 -0300

    atividade2

---
+++ Javascript/aula1-intro/atividades/atividade-aula1/sucessor.js
@@ -0,0 +1,5 @@
+var n1 = prompt("Digite um número:")
+var antecessor = Number(n1) - 1
+var sucessor = Number(n1) +1
+
+alert("O número antecessor é "+antecessor+", o número digitado é "+n1+" e o número sucessor é "+sucessor)
