ebbd159d9f15b473f8ac4eeca656c0516cd3ab9f
yas-afk <yasminsoaresfarias32@gmail.com>
2024-12-10 16:57:54 -0300

    atividade(10/12

--- Javascript/aula5-listas array/atividade/scripts/script.js
+++ Javascript/aula5-listas array/atividade/scripts/script.js
@@ -1,8 +1,25 @@
+nction testes(){
+    // enquanto a condicao faca
+    // para de tanto ate tanto faca
+    contador = 0
+
+          //  while(contador < 3){
+       //  alert(' que maneiro ' FIZ ${contador+1} vezes ') 
+    //   contador++ 
+ // }
+//    }
+for(var i = 0;1 < 3;i++){
+    alert(' que maneiro ` FIZ ${i+1} vezes')
+}
+}
+
+//testes()
+
 
 function carregarCatalogo(){
 
     var livros = [
-          ["images/a menina.jpg","A Menina que Roubava Livros"],
+          ["images/enrolados.jpg","A Menina que Roubava Livros"],
           ["images/jardim secreto.jpg", "Jardim Secreto"],
           ["images/juju.webp", "Jujustu Kaisen"],
           ["images/demonslayer.jpg","Kimetsu no yaiba"],
